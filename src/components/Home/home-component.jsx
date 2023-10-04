c019b58cae08f527b03bc6de46b7838c29c4f210
Squasmn <Squasmn.withem@gmail.com>
2023-10-04 18:21:55 +0200

    update paths

--- src/components/Home/home-component.jsx
+++ src/components/Home/home-component.jsx
@@ -3,7 +3,7 @@ import "./home-component.style.scss";
 export default function HomeComponent() {
   return (
     <div className="img-container">
-      <img src="src\assets\david\DavDevProfile2.png" />
+      <img src="./src\assets\david\DavDevProfile2.png" />
     </div>
   );
 }
